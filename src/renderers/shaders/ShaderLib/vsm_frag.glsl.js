6e8c29efe671cd6859547b313ac301fcb08d47bc
supereggbert <me@paulbrunt.co.uk>
2019-08-05 19:09:02 +0100

    crazy Adreno bug work around

--- src/renderers/shaders/ShaderLib/vsm_frag.glsl.js
+++ src/renderers/shaders/ShaderLib/vsm_frag.glsl.js
@@ -10,6 +10,8 @@ void main() {
   float mean = 0.0;
   float squared_mean = 0.0;
   
+  // This seems totally useless but it's a crazy work around for a Adreno compiler bug
+	float depth = unpackRGBAToDepth( texture2D( shadow_pass, ( gl_FragCoord.xy  ) / resolution ) );
 
   for ( float i = -1.0; i < 1.0 ; i += SAMPLE_RATE) {
 
@@ -28,7 +30,7 @@ void main() {
     #endif
 
   }
-
+      
   mean = mean * HALF_SAMPLE_RATE;
   squared_mean = squared_mean * HALF_SAMPLE_RATE;
 
